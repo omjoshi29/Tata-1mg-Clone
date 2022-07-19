1225e51198fdd54b893ec90ff10db6703daaae43
gautam6023 <gautamgohil0@gmail.com>
2022-07-20 00:23:39 +0530

    added styles setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Hello There</h1>
+      <p>React</p>
+    </div>
+  );
 }
 
 export default App;
