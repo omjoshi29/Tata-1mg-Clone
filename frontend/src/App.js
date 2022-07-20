a334ce69f2e0b36d406823e2a21c4b34483115fb
gautam6023 <gautamgohil0@gmail.com>
2022-07-20 23:48:00 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Cart } from "./components/Cart/cart";
-import Navigation from "./components/Navigation/Navigation"
+import Navigation from "./components/Navigation/Navigation";
+import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-     
+      <Navigation />
     </div>
   );
 }
