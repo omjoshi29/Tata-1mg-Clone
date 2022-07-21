0ab7600758346264813f152e49676e6ba86ac317
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-21 21:45:20 +0530

    address page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import { Cart } from "./components/Cart/cart";
 import Navigation from "./components/Navigation/Navigation";
 import Button from "./components/Button";
+import { Address } from "./components/Cart/address";
 function App() {
   return (
     <div className="App">
       {/* <Navigation /> */}
-      <Cart/>
+      {/* <Cart/> */}
+      <Address/>
     </div>
   );
 }
