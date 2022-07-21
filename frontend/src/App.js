7cc5b7e4ab685ff2875a38bd3006802262f0e508
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-07-21 23:25:39 +0530

    change app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,7 @@ function App() {
   return (
     <div className="App">
       {/* <Navigation /> */}
-      {/* <Cart/> */}
-      {/* <Address/> */}
-      <Delivery/>
+     
     </div>
   );
 }
