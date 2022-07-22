b39744e39d775753cc4bc9be044db24a2be23c86
gautam6023 <gautamgohil0@gmail.com>
2022-07-22 10:03:33 +0530

    update

--- frontend/src/components/Offer/Plan.jsx
+++ frontend/src/components/Offer/Plan.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import "./Plan.css"
+import React from "react";
+// import "./Plan.css"
 
 const Plan = () => {
-    return (
-        <div>
+  return (
+    <div>
+      <div id="navbar"></div>
+      {/* <img id="four" src={require("C:/Users/PARIMAL/OneDrive/Desktop/shocking-grade-9442/frontend/src/images/14.png")} alt="" /> */}
 
+      <img id="six" src={require("../images/16.png")} alt="" />
+      <img id="seven" src={require("../images/17.png")} alt="" />
+      <img id="eight" src={require("../images/18.png")} alt="" />
+      <img id="nine" src={require("../images/19.png")} alt="" />
+      <img id="ten" src={require("../images/20.png")} alt="" />
+      <img id="eleven" src={require("../images/21.png")} alt="" />
+      {/* {/* <!-- <img id="twelve" src="images/12.png" alt=""> --> */}
+      <div id="footer"></div>
+    </div>
+  );
+};
 
-            <div id="navbar"></div>
-            {/* <img id="four" src={require("C:/Users/PARIMAL/OneDrive/Desktop/shocking-grade-9442/frontend/src/images/14.png")} alt="" /> */}
-           
-            <img id="six" src={require('../images/16.png')} alt="" />
-            <img id="seven" src={require("../images/17.png")} alt="" />
-            <img id="eight" src={require("../images/18.png")} alt="" />
-            <img id="nine" src={require("../images/19.png")} alt="" />
-            <img id="ten" src={require("../images/20.png")} alt="" />
-            <img id="eleven" src={require("../images/21.png")} alt="" /> 
-            {/* {/* <!-- <img id="twelve" src="images/12.png" alt=""> --> */}
-            <div id="footer"></div>
-
-
-        </div>
-    )
-}
-
-export default Plan
+export default Plan;
